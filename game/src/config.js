dfaec579b36fdcf75dcf53f319070d3d8f7c3547
Ivan Lausuch <ilausuch@suse.com>
2018-07-07 16:20:05 +0200

    structure: fix initial structure

---
+++ game/src/config.js
@@ -0,0 +1,56 @@
+var phaser;
+
+// game configuration object
+var gameConfig = {
+
+  // render type
+  type: Phaser.AUTO,
+
+  // game width, in pixels
+  width: 800,
+
+  // game height, in pixels
+  height: 600,
+
+  // game background color
+  backgroundColor: 0x444444,
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 0 },
+        debug: true
+    }
+  },
+
+   // scenes used by the game
+   scene: [game]
+};
+
+// once the window loads...
+window.onload = function() {
+
+    // game constructor
+    phaser = new Phaser.Game(gameConfig);
+
+    // pure javascript to give focus to the page/frame and scale the game
+    window.focus()
+    resize();
+    window.addEventListener("resize", resize, false);
+}
+
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = phaser.config.width / phaser.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
