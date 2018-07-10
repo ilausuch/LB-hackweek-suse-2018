4aa0cfda41e813a20711cdef4c934910fc3cb319
Ivan Lausuch <ilausuch@suse.com>
2018-07-10 13:25:53 +0200

    Base object exteded with
    - screen object standarization
    - position of screen object
    - distance between objects

--- game/src/factory/BaseObject.js
+++ game/src/factory/BaseObject.js
@@ -1,9 +1,15 @@
 class BaseObject{
-  constructor(scene, id){
+  constructor(scene, id, multiple){
     this.scene = scene;
+<<<<<<< Updated upstream
     
     if (id == undefined)
       this.id = Math.random();
+=======
+
+    if (multiple)
+      this.id = id + "_" + Math.ceil(10000 * Math.random());
+>>>>>>> Stashed changes
     else
       this.id = id;
 
@@ -22,7 +28,36 @@ class BaseObject{
 
   }
 
+<<<<<<< Updated upstream
+=======
+  delayedCreation(){
+
+  }
+
+>>>>>>> Stashed changes
   update(){
 
+  }
+
+  get_screen_object(){
+    //By default is object, but must be changed
+    return this.object;
+  }
+
+  position(center){
+    var object = this.get_screen_object();
+
+    if (center === true)
+      return {x:object.x + object.width/2, y:object.y + object.height/2};
+    else
+      return {x:object.x, y:object.y};
+  }
+
+  distance_to_object(object){
+    var position = this.position(true);
+    var position2 = object.position(true);
+
+    return (position.x - position2.x) * (position.x - position2.x)
+           + (position.y - position2.y) * (position.y - position2.y)
   }
 }
--- game/src/factory/hero.js
+++ game/src/factory/hero.js
@@ -21,12 +21,12 @@ class Hero extends BaseObject {
   }
 
   prepareObjects() {
-    this.heroObj = this.scene.physics.add.sprite(150, 0 , 'hero');
-    this.heroObj.setVelocity(0, 200);
-    this.heroObj.setBounce(0, 0);
-    this.heroObj.setCollideWorldBounds(true);
-    this.heroObj.setGravityY(1000);
-    this.heroObj.y = this.scene.objects.scenario.data.floor - this.heroObj.height;
+    this.object = this.scene.physics.add.sprite(150, 0 , 'hero').setScale(0.9);
+    this.object.setVelocity(0, 200);
+    this.object.setBounce(0, 0);
+    this.object.setCollideWorldBounds(true);
+    this.object.setGravityY(1000);
+    this.object.y = this.scene.objects.scenario.data.floor - this.object.height;
   }
 
   prepareSounds(){
@@ -36,27 +36,41 @@ class Hero extends BaseObject {
   prepareControls(){
     this.cursors = this.scene.input.keyboard.createCursorKeys();
     this.scene.input.keyboard.on('keydown_SPACE', function(event) {
-        if (this.heroObj.body.velocity.y == 0)
-          this.heroObj.setVelocity(0,-500);
+        if (this.object.body.velocity.y == 0)
+          this.object.setVelocity(0,-500);
       }, this);
   }
 
   postCreation() {
-    this.scene.scenario.define_collisions(this.heroObj);
+    this.scene.scenario.define_collisions(this.object);
+    var $this = this;
+    setTimeout(function(){
+      //$this.object.body.width = 50;
+    })
   }
 
 
   update() {
-    this.heroObj.setVelocityX(0);
+    this.object.setVelocityX(0);
+    if (this.cursors.up.isDown && this.object.body.velocity.y == 0)
+    {
+        this.object.setVelocityY(-500);
+    }
     if (this.cursors.left.isDown)
     {
-        this.heroObj.setVelocityX(-300);
-        this.heroObj.flipX = true;
+        this.object.setVelocityX(-300);
+        this.object.flipX = true;
     }
     else if (this.cursors.right.isDown)
     {
-        this.heroObj.setVelocityX(300);
-        this.heroObj.flipX = false;
+        this.object.setVelocityX(300);
+        this.object.flipX = false;
+    }
+    if (this.object.flipX) {
+      //this.object.body.x = this.object.x + this.object.width*0.5 - 90;
+    }
+    else {
+      //this.object.body.x = this.object.x - this.object.width*0.5+35;
     }
   }
 
