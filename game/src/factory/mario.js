6c701a9cfd8a9e619ed6e106adb1de0723a0846a
Jose Lausuch <jalausuch@suse.com>
2018-07-11 23:54:38 +0200

    Add Mario attack

--- game/src/factory/mario.js
+++ game/src/factory/mario.js
@@ -7,18 +7,19 @@ class Mario extends BaseObject {
     this.posY = posY;
     this.speed = speed;
     this.collide_with_walls = true;
+    this.isAttacking = false;
+    this.attackSpeed = 350;
   }
 
   preload() {
     //this.scene.load.image('hero', '/assets/img/characters/hero/base.png');
-    /*
-    this.scene.load.spritesheet("hero_attack", 'assets/img/characters/hero/attack.png', {
-      frameWidth: 94,
-      frameHeight: 35,
+
+    this.scene.load.spritesheet("mario_attack", 'assets/img/characters/mario/attack.png', {
+      frameWidth: 11,
+      frameHeight: 11,
       margin: 0,
       spacing: 0
-    });*/
-    //sprites attack: 94x35
+    });
 
     this.scene.load.spritesheet("mario", 'assets/img/characters/mario/walk.png', {
       frameWidth: 44.5,
@@ -38,17 +39,16 @@ class Mario extends BaseObject {
   };
 
   createAnimations() {
-    /*
     this.scene.anims.create({
-      key: 'hero_attack',
-      frames: this.scene.anims.generateFrameNumbers('hero_attack', {
+      key: 'mario_attack',
+      frames: this.scene.anims.generateFrameNumbers('mario_attack', {
         start: 0,
-        end: 9
+        end: 3
       }),
       scale: 1,
       frameRate: 15,
-      repeat: 0
-    });*/
+      repeat: -1
+    });
     this.scene.anims.create({
       key: 'mario',
       frames: this.scene.anims.generateFrameNumbers('mario', {
@@ -72,6 +72,17 @@ class Mario extends BaseObject {
     this.object.play("mario");
     this.object.anims.play();
     this.object.owner = this;
+
+    this.mario_attack = this.scene.physics.add.sprite(0, 0, 'mario_attack').setScale(1.5);
+    this.mario_attack.setVelocity(this.attackSpeed, 0);
+    this.mario_attack.setBounce(1, 0.6);
+    this.mario_attack.setCollideWorldBounds(false);
+    this.mario_attack.setGravityY(1000);
+    this.mario_attack.x = 0;
+    this.mario_attack.y = 0;
+    this.mario_attack.play("mario_attack");
+    this.mario_attack.anims.play();
+    this.mario_attack.owner = this;
   }
 
   prepareSounds(){
@@ -85,6 +96,7 @@ class Mario extends BaseObject {
     var $this = this;
     this.scene.scenario.define_collisions(this.object, undefined, this.onCollideWalls);
     this.scene.scenario.define_collisions_platforms(this.object, this.onCollidePlatforms);
+    this.scene.scenario.define_collisions(this.mario_attack, undefined, undefined, "floor");
     setTimeout(function(){
       //$this.object.body.width = 50;
     })
@@ -94,12 +106,10 @@ class Mario extends BaseObject {
     a.owner.speed = -1 * a.owner.speed;
     a.setVelocityX(a.owner.speed);
   }
+
   onCollidePlatforms(a,b) {
     var prob = Math.floor((Math.random() * 100) + 1);
     if (prob > 80) {
-        //a.owner.speed = -1 * a.owner.speed;
-        //a.setVelocityX(a.owner.speed);
-        console.log(prob);
         a.owner.collide_with_walls = false;
         setTimeout(function(){
           a.owner.collide_with_walls = true;
@@ -116,6 +126,26 @@ class Mario extends BaseObject {
     var $this = this;
     if (this.object.body.velocity.x >0) this.object.flipX = false;
     else this.object.flipX = true;
+    if (Math.floor((Math.random() * 100) + 1) > 50) {
+      if (! this.isAttacking) {
+        console.log("MARIO ATTACK!")
+        this.isAttacking = true;
+        this.attack();
+        setTimeout(function(){
+          $this.isAttacking = false;
+        },4000)
+      }
+    }
+  }
+
+  attack() {
+    this.mario_attack.setVelocity(0, 0);
+    this.mario_attack.x = this.object.x;
+    this.mario_attack.y = this.object.y - 25;
+    var speed = this.attackSpeed;
+    if (this.object.flipX) speed = - this.attackSpeed;
+    this.mario_attack.setVelocity(speed, 0);
+
   }
 
 }
