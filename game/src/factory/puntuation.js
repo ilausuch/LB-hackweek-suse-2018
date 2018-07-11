fb7b29b258a732dd3028ae1b5d60ed3d68c22bc8
Ivan Lausuch <ilausuch@suse.com>
2018-07-11 18:18:55 +0200

    Added fonts and puntuation system

---
+++ game/src/factory/puntuation.js
@@ -0,0 +1,18 @@
+class Puntuation extends BaseObject{
+  constructor(scene, font){
+    super(scene, "puntuation");
+    this.font = font;
+  }
+
+  create(){
+    this.characters = this.font.addString("000", 350, 20, 0.5);
+  }
+
+  update_puntuation(){
+    this.characters[0].setCharacter("" + Math.floor(gameStatus.puntuation/100));
+    this.characters[1].setCharacter("" + Math.floor((gameStatus.puntuation%100)/10));
+    this.characters[2].setCharacter("" + Math.floor(gameStatus.puntuation%10));
+
+
+  }
+}
