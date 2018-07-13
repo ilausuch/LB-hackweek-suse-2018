8e8bb831e067d10d60c713f1b4aac7ee6f1327a1
Ivan Lausuch <ilausuch@suse.com>
2018-07-13 10:10:08 +0200

    Enemies parameters defined in a config & pacman +

--- game/src/factory/bug1Hive.js
+++ game/src/factory/bug1Hive.js
@@ -1,12 +1,7 @@
 class Bug1 extends BugBase{
   constructor(scene, hive, initial_position) {
     super(scene, hive, initial_position);
-    this.max_speed=100;
-    this.chaise_speed=120;
-    this.attack_distance = 40000;
-    this.acceleration =20;
-    this.puntuation = 10;
-    this.decrease_energy_on_bite = 0.01;
+    this.configure("bug1");
   }
 }
 
